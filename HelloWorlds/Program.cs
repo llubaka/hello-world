2e0b7d5bb9ad0f058873635cb4583bb08c6e8d9c
llubaka <llubaka@abv.bg>
2019-01-18 00:24:28 +0200

    hello

---
+++ HelloWorlds/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HelloWorlds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hw = "Hello world!";
+
+            PrintHelloWorld(hw);
+
+            Console.ReadLine();
+        }
+
+        private static void PrintHelloWorld(string hw)
+        {
+            foreach (var c in hw)
+            {
+                Console.Write(c);
+                Thread.Sleep(60);
+            }
+            Console.Clear();
+
+            for (int i = 0; i < hw.Length; i++)
+            {
+                Console.Clear();
+                Console.WriteLine(hw.Remove(hw.Length - 1 - i));
+
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
